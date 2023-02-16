c879b5aabe4fb5ffa4ac1f03856de7288a29ab1d
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-02-16 16:12:19 +0100

    schema product

--- database/migrations/2023_02_16_142655_create_products_table.php
+++ database/migrations/2023_02_16_142655_create_products_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
+            $table->string('name', 100);
+            $table->string('type', 100);
+            $table->text('description');
+            $table->decimal('price', 5.2);
+            $table->string('image')->nullable();
+            $table->boolean('is_available')->default(true);
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
