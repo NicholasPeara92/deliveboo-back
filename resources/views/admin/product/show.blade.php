d6e07fdc6ca5a41be6fb7f6095a3a56d790472fb
NicholasPeara92 <nicholas.peara@gmail.com>
2023-02-17 16:37:42 +0100

    Aggiunge immagine alla show

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -3,6 +3,14 @@
 @section('content')
   <div class="container">
     <a href="{{ route('admin.product.index', $product->id) }}" class="btn btn-success">Menù</i></a>
+    <div class="mt-4">
+      <div class="text-center">
+        @if ($project->cover_image)
+          <img class="w-25" src="{{ asset("storage/$project->cover_image") }}" alt="{{ $project->title }}">
+        @endif
+      </div>
+      {{ $project->description }}
+    </div>
     <h1>{{ $product->name }}</h1>
     {{-- imgage --}}
     <h3>{{ $product->type }}</h3>
