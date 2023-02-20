45607e7b4fc5b7feab4db3153c5520dbcf03506b
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-02-20 16:14:23 +0100

    Modifiche estetiche show

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -2,20 +2,18 @@
 
 @section('content')
   <div class="container">
-    <a href="{{ route('admin.product.index', $product->id) }}" class="btn btn-success">Menù</i></a>
-    <div class="mt-4">
-      <div class="text-center">
+    <div class="txt-center">
+      <h1>Nome prodotto: {{ $product->name }}</h1>
         @if ($product->image)
           <img class="w-25" src="{{ asset("storage/$product->image") }}" alt="{{ $product->name }}">
         @endif
-      </div>
     </div>
 
-    <h1>Nome prodotto: {{ $product->name }}</h1>
     {{-- imgage --}}
     <h3>Tipologia del prodotto: {{ $product->type }}</h3>
     <h4>Costo: {{ $product->price }} €</h4>
     <p>Descrizione: {{ $product->description, $product->is_available }}
     </p>
+    <a href="{{ route('admin.product.index', $product->id) }}" class="mt-3 btn btn-success">Menù</i></a>
   </div>
 @endsection
