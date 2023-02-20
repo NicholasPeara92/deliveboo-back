9342e90be028b52177b696abc7b17a0dbc0bf8e7
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-02-20 16:44:37 +0100

    Modificata index di resturant + modifiche minori per show di product

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -3,16 +3,16 @@
 @section('content')
   <div class="container">
     <div class="txt-center">
-      <h1>Nome prodotto: {{ $product->name }}</h1>
+      <h1><strong>Nome prodotto:</strong> {{ $product->name }}</h1>
         @if ($product->image)
           <img class="w-25" src="{{ asset("storage/$product->image") }}" alt="{{ $product->name }}">
         @endif
     </div>
 
     {{-- imgage --}}
-    <h3>Tipologia del prodotto: {{ $product->type }}</h3>
-    <h4>Costo: {{ $product->price }} €</h4>
-    <p>Descrizione: {{ $product->description, $product->is_available }}
+    <h3><strong>Tipologia del prodotto:</strong> {{ $product->type }}</h3>
+    <h4><strong>Costo:</strong> {{ $product->price }} €</h4>
+    <p><strong>Descrizione:</strong> {{ $product->description, $product->is_available }}
     </p>
     <a href="{{ route('admin.product.index', $product->id) }}" class="mt-3 btn btn-success">Menù</i></a>
   </div>
--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -2,9 +2,18 @@
 
 @section('content')
     <div class="container">
-        <h1 class="my-3">{{ $restaurant->name}}</h1>
-        <a href="{{ route('admin.restaurant.edit', $restaurant) }}" class="btn btn-warning"><i class="fa-solid fa-pen"></i>Modifica il tuo ristorante</a>
-    {{-- gestione degli errori di validazione --}}
+        <h1 class="my-3"><strong>Nome del Ristorante: </strong>{{ $restaurant->name}}</h1>
+        <h2><strong>Numero di telefono:</strong> +39 {{ $restaurant->telephone }}</h2>
+        <h2><strong>Indirizzo:</strong> {{ $restaurant->address }}</h2>     
+        <h2><strong>Partita IVA:</strong> {{ $restaurant->iva }}</h2>
+        
+        {{-- bottone edit --}}
+        <a href="{{ route('admin.restaurant.edit', $restaurant) }}" class="mt-3 me-4 btn btn-warning"><i class="fa-solid fa-pen"></i>Modifica il tuo ristorante</a>
+        
+        {{-- bottone delete --}}
+        <a href="{{ route('admin.restaurant.destroy', $restaurant->id) }}" class="mt-3 btn btn-danger"><i class="fa-solid fa-trash"></i><i>Elimina il tuo ristorante</i></a>
+        
+        {{-- gestione degli errori di validazione --}}
         @if ($errors->any())
         <div class="alert alert-danger">
             <ul class="list-unstyled">
@@ -15,6 +24,7 @@
         </div>
         @endif
         {{-- /gestione degli errori di validazione --}}
+       
         @if(session('message'))
         <div class="alert alert-danger">
             <ul class="list-unstyled">
