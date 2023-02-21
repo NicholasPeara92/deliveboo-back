f8a19286d0d0b4653e4a7ac92b68cfed578b2a71
NicholasPeara92 <nicholas.peara@gmail.com>
2023-02-21 12:28:23 +0100

    Aggiunge controlli campi input ad edit e create

--- resources/views/admin/restaurant/create.blade.php
+++ resources/views/admin/restaurant/create.blade.php
@@ -18,41 +18,42 @@
       <form action="{{ route('admin.restaurant.store') }}" method="POST" enctype="">
         @csrf
         <div class="mb-3">
-          <label for="name" class="form-label">Nome Ristorante</label>
+          <label for="name" class="form-label">Nome Ristorante*</label>
           <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci il nome"
-            value="{{ old('name') }}">
+            value="{{ old('name') }}" required>
         </div>
         <div class="mb-3">
-          <label for="address" class="form-label">Indirizzo</label>
-          <input type="text" class="form-control" id="address" name="address"
-            placeholder="Inserisci l'indirizzo" {{ old('address') }}>
+          <label for="address" class="form-label">Indirizzo*</label>
+          <input type="text" class="form-control" id="address" name="address" placeholder="Inserisci l'indirizzo"
+            {{ old('address') }} required>
         </div>
         <div class="mb-3">
-          <label for="iva" class="form-label">Partita IVA</label>
-          <input type="text" class="form-control" id="iva" name="iva"
-            placeholder="Inserisci la Partita IVA" {{ old('iva') }}>
+          <label for="iva" class="form-label">Partita IVA*</label>
+          <input type="text" class="form-control" id="iva" name="iva" placeholder="Inserisci la Partita IVA"
+            {{ old('iva') }} required>
         </div>
         <div class="mb-3">
-          <label for="telephone" class="form-label">Inserisci il numero di telefono</label>
+          <label for="telephone" class="form-label">Inserisci il numero di telefono*</label>
           <input type="text" class="form-control" id="telephone" name="telephone"
-            placeholder="Inserisci il numero di telefono" {{ old('telephone') }}>
+            placeholder="Inserisci il numero di telefono" {{ old('telephone') }} required>
         </div>
 
         <div class="mb-3">
-            <label for="image" class="form-label">Immagine</label>
-            <div class="mb-2">
-              <script>
-                  var loadFile = function(event) {
-                  var output = document.getElementById('output');
-                  output.src = URL.createObjectURL(event.target.files[0]);
-                  output.onload = function() {
-                      URL.revokeObjectURL(output.src) // free memory
-                  }
-                  };
-              </script>
-            </div>
-            <img width="100" id="output">
-            <input type="file" class="form-control" id="image" name="image" value="{{old('image')}}" onchange="loadFile(event)">          
+          <label for="image" class="form-label">Immagine</label>
+          <div class="mb-2">
+            <script>
+              var loadFile = function(event) {
+                var output = document.getElementById('output');
+                output.src = URL.createObjectURL(event.target.files[0]);
+                output.onload = function() {
+                  URL.revokeObjectURL(output.src) // free memory
+                }
+              };
+            </script>
+          </div>
+          <img width="100" id="output">
+          <input type="file" class="form-control" id="image" name="image" value="{{ old('image') }}"
+            onchange="loadFile(event)">
         </div>
         {{-- <div class="mb-3">
                     @foreach ($categories as $technology)
