46515d1dec040931fb82e982936a794c989adab8
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-02-21 12:55:59 +0100

    sistemazione category e order seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 class CategorySeeder extends Seeder
 {
@@ -15,6 +16,11 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
+        //cancello tutti i dati della tabella categories
+        Category::truncate();
+        Schema::enableForeignKeyConstraints();
+
         $categories = ['Italiano', 'Asiatico', 'Fast Food', 'Messicano', 'Pizza', 'Kebab', 'Gelateria'];
 
         foreach ($categories as $category) {
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Order;
+use Illuminate\Support\Facades\Schema;
 
 class OrderSeeder extends Seeder
 {
@@ -12,8 +15,23 @@ class OrderSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(Faker $faker)
+    {   
+        Schema::disableForeignKeyConstraints();
+        //cancello tutti i dati della tabella categories
+        Order::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        for ($i=0; $i < 100 ; $i++) { 
+            $new_order = new Order();
+            $new_order->name = $faker->name();
+            $new_order->surname = $faker->lastName();
+            $new_order->address = $faker->streetAddress();
+            $new_order->telephone = '3'.$faker->randomNumber(9, true);
+            $new_order->email = preg_replace('/@example..*/', '@gmail.com', $faker->unique()->safeEmail);
+            $new_order->total = $faker->randomFloat(2, 1, 999);
+            $new_order->create_order = $faker->dateTimeBetween('-1 year', '0 days');
+            $new_order->save();
+        }
     }
 }
