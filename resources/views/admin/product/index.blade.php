f10ceafa17d9c241b0b781bab298e84d4a36c939
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-02-21 18:49:17 +0100

    Modale destroy product

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -23,14 +23,34 @@
           <td>{{ $product->description }}</td>
           <td>{{ $product->price }}</td>
           <td width="170">
+            {{-- Show --}}
             <a href="{{ route('admin.product.show', $product->id) }}" class="btn btn-success"><i
                 class="fa-solid fa-eye"></i></a>
+            {{-- Edit --}}
             <a href="{{ route('admin.product.edit', $product->id) }}" class="btn btn-warning"><i
                 class="fa-solid fa-pencil"></i></a>
-            <form action="{{ route('admin.product.destroy', $product) }}" method="POST" class="d-inline-block">
+              {{-- Destroy --}}
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{ $product->id }}">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+          </td>
+        </tr>
+        <div class="modal fade" id="modal-{{ $product->id }}" tabindex="-1">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Sei sicuro di eliminare il prodotto "{{ $product->name }}"?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                <form action="{{ route('admin.product.destroy', $product) }}" method="POST" class="d-inline-block">
               @csrf
               @method('DELETE')
-              <button type="submit" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+              <button type="submit" class="btn btn-danger">Si</button>
             </form>
           </td>
         </tr>
