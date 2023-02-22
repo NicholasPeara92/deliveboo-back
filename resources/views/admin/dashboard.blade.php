fbe34086f7052b6d2f0be239d430b6935e5ce2b8
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-02-22 10:07:46 +0100

    Aggiunto modifiche dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h3 class="my-3"><strong>DASHBOARD</strong></h3>
+    <div>
+        <h1 class="my-3"><strong>DASHBOARD</strong></h1>
+        <h3>Benvenuto {{ $restaurant->name }}</h3>
+    </div>
 @endsection
