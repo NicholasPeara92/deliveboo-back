47756dcc865c2fcbd262425f05f666c542af605b
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-02-22 10:20:40 +0100

    condizione restaurant

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div>
         <h1 class="my-3"><strong>DASHBOARD</strong></h1>
-        <h3>Benvenuto {{ $restaurant->name }}</h3>
+        @if ($restaurant)
+            <h3>Benvenuto {{ $restaurant->name }}</h3>
+        @endif
+
     </div>
 @endsection
