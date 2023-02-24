788c9c1b68dcd87dbee2d515914cfdc1e4ff334b
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-02-24 17:00:57 +0100

    product show

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <div class="card"
+        <div class="card p-4"
             style="max-width: 500px; border: 2px solid #ccc; border-radius: 10px; box-shadow: 0px 2px 6px #ccc;">
             <img src="{{ asset("storage/$product->image") }}" class="card-img-top" alt="{{ $product->name }}"
-                style="height: 350px; object-fit: cover;">
+                style="height: 350px; width: 350px; object-fit: cover;">
             <div class="card-body">
                 <h5 class="card-title"
                     style="font-family: Arial, sans-serif; font-size: 2rem; font-weight: bold; margin-bottom: 0.5rem;">
