8cb58466c0e5aa8fa1591651e04ef2a4f696863a
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-03-01 18:52:53 +0100

    creazione ordine back

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index(){
+        
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:100', 
+            'surname' => 'required|string|max:50',
+            'email' => 'required|string',
+            'telephone' => 'required|string|max:10', 
+            'total' => 'required|decimal:2',
+            'address' => 'required|string'
+        ]);
+        $data = $request->all();
+
+        $new_order = new Order();
+        $new_order->name = $data['name'];
+        $new_order->surname = $data['surname'];
+        $new_order->address = $data['address'];
+        $new_order->telephone = $data['telephone'];
+        $new_order->total = $data['total'];
+        $new_order->email = $data['email'];
+        $new_order->create_order = now();
+        $new_order->save();
+    }
+}
